5ffa71b9895a70d1dd1ff6ef308d86d283034793
Marcos <marcosvidal@outlook.com>
2021-06-04 20:49:23 -0300

    Initial commit

---
+++ DIO.desafio.listaserie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.desafio.listaserie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
